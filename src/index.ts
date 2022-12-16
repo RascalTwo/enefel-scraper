8d7de4b3dbb0b9888f6bcb6fbba629ab300c322d
Castle <castlerock88@protonmail.com>
2022-12-16 13:03:46 -0500

    refactor: route paths

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,9 @@
 import express from "express";
 import teamsRouter from "./router/teamRouter.js";
 import playerRouter from "./router/playerRotuer.js";
-import scheduleRouter from "./router/scheduleRouter.js";
-
-import {
-  getData,
-  getPlayerStats,
-  getTeamRosters,
-  getTeamStats,
-} from "./utils/services.js";
-import { teamsUrl } from "./utils/consts.js";
-import { teamScraper } from "./utils/scrapers.js";
-import prisma from "./utils/db.js";
 
 const app = express();
 const port = 5000;
-
-// app.use("/", scheduleRouter);
-app.get("/", async (req, res) => {
-  const teams = await getPlayerStats();
-  res.json({ teams });
-});
 
 app.use("/teams", teamsRouter);
 
