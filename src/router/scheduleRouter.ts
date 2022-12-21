2cab3fb8b8cf7c0e9bb978c34f4760117a906699
Castle <castlerock88@protonmail.com>
2022-12-21 14:15:42 -0500

    refactor: cleanup for deploy

--- src/router/scheduleRouter.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getSchedule } from "../controller/scheduleController.js";
-const scheduleRouter = Router();
-
-scheduleRouter.get("/", getSchedule);
-
-export default scheduleRouter;
